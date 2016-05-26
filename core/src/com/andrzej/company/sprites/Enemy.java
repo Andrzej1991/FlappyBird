65e41e1199e0ca348a2943fb3146424647574c67
Andrzej1991 <andrzej.perski@hotmail.com>
2016-05-26 18:51:22 +0200

    Flappy bird game making on LibGDX framework

---
+++ core/src/com/andrzej/company/sprites/Enemy.java
@@ -0,0 +1,17 @@
+package com.andrzej.company.sprites;
+
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by Andrzej on 2016-04-30.
+ */
+public class Enemy {
+    private Texture andromeda;
+    private Music music;
+    private Date data;
+    private Random rand;
+}
